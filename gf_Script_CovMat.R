d68d21a38c0b8c3d0c2776d4f13946cee8ee0492
Delia Fuhrmann <dfuhrmann.mail@gmail.com>
2020-01-24 17:19:17 +0000

    Update gf_Script_CovMat.R

--- gf_Script_CovMat.R
+++ gf_Script_CovMat.R
@@ -4,9 +4,9 @@
 
 # Fuhrmann, D., Simpson-Kent, I.L., Bathelt, J., the CALM team & Kievit, R. A.
 #    (2019). A hierarchical watershed model of fluid intelligence in childhood and adolescence.
-#    bioRxiv 435719, doi: https://doi.org/10.1101/435719 
+#    Cerebral Cortex, bhz091, https://doi.org/10.1093/cercor/bhz091
 
-# The script uses covariance matrices for data from two samples: CALM and NKI-RS. 
+# The script uses the covariance matrix fromt he CALM sample. 
 # Estimates and test statistics will differ from values reported in the paper due to missingness. 
 # To reproduce results exactly, please request raw data from http://fcon_1000.projects.nitrc.org/indi/enhanced/data.html 
 # for NKI-RS. Raw data for CALM has not yet been released due to ethics restrictions. Please check
@@ -25,7 +25,6 @@ library(lavaan)
 
 ### Load covariance matrix
 cov.matrix.CALM = read.csv("CovMat_CALM.csv", row.names = 1) # CALM data
-cov.matrix.NKI  = read.csv("CovMat_NKI.csv", row.names = 1)  # NKI-RS data
 
 ################################################################################
 ### Measurement model of the watershed model for both samples
@@ -40,17 +39,6 @@ PS =~ PS_PhAB_il + PS_Teach_il + PS_DKEFS_il
 fit_ThreeFactor_CALM <- cfa(model_ThreeFactor_CALM, sample.cov=as.matrix(cov.matrix.CALM), 
                             sample.nobs=551, std.lv=T)
 summary(fit_ThreeFactor_CALM, standardized = T)
-
-model_ThreeFactor_NKI<-
-  '
-# latent variables
-gf =~ wasi_block + wasi_matrix + wasi_sim + penn_verbal
-WM =~ penn_nback + ds_f + ds_b
-PS =~ penn_smspeed + dkefs_rt + penn_mspeed
-'
-fit_ThreeFactor_NKI <- cfa(model_ThreeFactor_NKI, sample.cov=as.matrix(cov.matrix.NKI), 
-                           sample.nobs=335, std.lv=T)
-summary(fit_ThreeFactor_NKI, standardized = T)
 
 ################################################################################
 ### MIMIC models (modelling cognitive levels of the watershed model)
@@ -68,20 +56,6 @@ gf ~ PS + WM
 fit_MIMIC_CALM <- sem(model_MIMIC_CALM, sample.cov=as.matrix(cov.matrix.CALM), 
                           sample.nobs=551, std.lv=T)
 summary(fit_MIMIC_CALM, standardized = T)
-
-model_MIMIC_NKI <-
-  '
-# latent variables
-gf =~ wasi_block + wasi_matrix + wasi_sim + penn_verbal
-WM =~ penn_nback + ds_f + ds_b
-PS =~ penn_smspeed + dkefs_rt + penn_mspeed 
-
-# regressions
-gf ~ PS + WM
-'
-fit_MIMIC_NKI <- sem(model_MIMIC_NKI, sample.cov=as.matrix(cov.matrix.NKI), 
-                         sample.nobs=335, std.lv=T)
-summary(fit_MIMIC_NKI, standardized = T)
 
 ################################################################################
 ### Full watershed models including neuroimaging data (FA in 10 different white matter tracts)
@@ -104,22 +78,3 @@ PS ~~ WM
 fit_watershed_CALM <- sem(model_watershed_CALM, sample.cov=as.matrix(cov.matrix.CALM), 
                           sample.nobs=551, std.lv=T, fixed.x=F)
 summary(fit_watershed_CALM, standardized = T)
-
-model_watershed_NKI <-
-  '
-# latent variables
-gf =~ wasi_block + wasi_matrix + wasi_sim + penn_verbal
-WM =~ penn_nback + ds_f + ds_b
-PS =~ penn_smspeed + dkefs_rt + penn_mspeed 
-
-# regressions
-gf ~ PS + WM
-PS ~ fa_UF + fa_SLF + fa_IFOF + fa_ATR + fa_CST + fa_FMaj + fa_FMin + fa_CG + fa_CH + fa_ILF
-WM ~ fa_UF + fa_SLF + fa_IFOF + fa_ATR + fa_CST + fa_FMaj + fa_FMin + fa_CG + fa_CH + fa_ILF
-
-# covariances
-PS ~~ WM
-'
-fit_watershed_NKI <- sem(model_watershed_NKI, sample.cov=as.matrix(cov.matrix.NKI), 
-                         sample.nobs=335, std.lv=T, fixed.x=F)
-summary(fit_watershed_NKI, standardized = T)
